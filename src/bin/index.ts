0dbdb9e85e81b1ed61f4135f06882afdb5488fa9
rahil <rahil.kabani.4@gmail.com>
2021-06-28 07:53:42 +0530

    feat: init

---
+++ src/bin/index.ts
@@ -0,0 +1,31 @@
+import { program } from "commander";
+import { readFile } from "fs";
+import { join } from "path";
+import { exit } from "process";
+import justEcho from "../lib/index";
+
+readFile(join(__dirname, "..", "..", "package.json"), (err, pkg) => {
+  if (err) {
+    process.stderr.write(`${err.message}\n`);
+    exit(1);
+  }
+
+  const { name, description, version } = JSON.parse(pkg.toString()) as {
+    name: string;
+    description: string;
+    version: string;
+  };
+
+  program
+    .name(`npx ${name}`)
+    .version(version)
+    .arguments("[args...]")
+    .description(description, {
+      args: "echoes (prints) the provided arguments to stdout",
+    })
+    .action((args: string[]) => {
+      process.stdout.write(`${justEcho(...args).join(" ")}\n`);
+    });
+
+  program.parse();
+});
