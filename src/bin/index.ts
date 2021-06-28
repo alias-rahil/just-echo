ad259ae52bbf0332422f1916c4c9434001201c71
rahil <rahil.kabani.4@gmail.com>
2021-06-28 07:53:42 +0530

    feat: init

---
+++ src/bin/index.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+
+import { program } from "commander";
+import justEcho from "../lib/index";
+import pkg from "../../package.json";
+
+const { name, description, version } = pkg;
+
+program
+  .name(`npx ${name}`)
+  .version(version)
+  .arguments("[args...]")
+  .description(description, {
+    args: "echoes (prints) the provided arguments to stdout",
+  })
+  .action((args: string[]) => {
+    console.log(justEcho(...args).join(" "));
+  });
+
+program.parse();
